57cb3ab3570acdf2b179ff61d7eece78c8b7c226
Scissordragonboy <Mohzuned@gmail.com>
2019-03-02 07:50:50 +0000

    aosp-ify

--- aosp_h930.mk
+++ aosp_h930.mk
@@ -1,14 +1,14 @@
 $(call inherit-product, device/lge/h930/full_h930.mk)
 
-# Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+# Inherit some common Aosp-Extended stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device identifiers
 
-PRODUCT_NAME := havoc_h930
+PRODUCT_NAME := aosp_h930
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V30
